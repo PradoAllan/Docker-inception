7b0d267a3ba445519c3a70dd5404bfd4f6b7d401
PradoAllan <allanprado.it@gmail.com>
2025-05-23 19:08:12 -0300

    docs: :memo: update files for AULA13 dia 2

--- dockerfiles/4/hello.c
+++
@@ -1,6 +0,0 @@
-#include "stdio.h"
-
-int main(void)
-{
-    printf("aprado is studying some container and Docker concepts to do Inception projects.\n");
-}
--- dockerfiles/4/meu_go.go
+++ dockerfiles/4/meu_go.go
@@ -1,7 +1,6 @@
 package main
 import "fmt"
 
-func main()
-{
+func main() {
 	fmt.Println("aprado learning Docker and container for Inception...")
 }
